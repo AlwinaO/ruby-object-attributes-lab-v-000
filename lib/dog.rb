e395611ff16be6eb3e1b8320d32225049436f975
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-09-20 00:43:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,9 +7,22 @@ class Dog
     @name
   end
 
+  def breed=(dog_type)
+    @breed = dog_type
+  end
+
+  def breed
+    @breed
+  end
+
 end
 
 fido = Dog.new
 fido.name = "Fido"
 
 puts fido.name
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+
+puts snoopy.breed
